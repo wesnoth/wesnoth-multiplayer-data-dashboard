68ba5c5c59eb721202a2a13d0f5a6cb25d2211fe
Alexander Lacson <41433185+max-torch@users.noreply.github.com>
2023-09-28 20:20:55 +0800

    Update docstring of main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """
 This script creates and runs the Flask application for the Wesnoth MP Database Dashboard.
+This is used to start the production server. For development, use `python app.py` instead.
 
 Usage:
 
@@ -7,7 +8,7 @@ Usage:
 
 Where $PORT specifies the port number
 
-Note: Gunicorn only runs on Linux/Unix systems. For Windows, use `waitress` instead.
+Note: Gunicorn only runs on Linux/Unix systems. For Windows, use `waitress` instead, or use a Linux container/VM to run Gunicorn.
 """
 
 from app import create_app
