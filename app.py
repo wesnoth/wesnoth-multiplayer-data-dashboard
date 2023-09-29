5aa0145dcbd7dc92a821c62494c098d67dbb82cc
Alexander Lacson <41433185+max-torch@users.noreply.github.com>
2023-09-30 02:12:35 +0800

    Format document

--- app.py
+++ app.py
@@ -26,14 +26,15 @@ def connect_to_mariadb():
 
 def create_app():
     app = Dash(
-        __name__, 
+        __name__,
         external_stylesheets=[
-        "https://www.wesnoth.org/wesmere/css/wesmere-1.1.10.css",
-        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+            "https://www.wesnoth.org/wesmere/css/wesmere-1.1.10.css",
+            "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         ],
         title="Wesnoth Multiplayer Dashboard",
         meta_tags=[
-            {'name': 'description', 'content': 'A dashboard for a database of Wesnoth multiplayer games.'}
+            {'name': 'description',
+                'content': 'A dashboard for a database of Wesnoth multiplayer games.'}
         ]
     )
 
