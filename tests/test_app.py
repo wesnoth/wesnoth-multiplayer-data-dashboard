0f835ccf0928c7cfa4b12737f3b683cc4d1af3f6
Alexander Lacson <lacsonalexanderz@gmail.com>
2024-03-20 03:33:28 +0800

    Update test_app.py
    Restore update tests for table and histogram

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 import pytest
 from dash.exceptions import PreventUpdate
+from pandas import NaT, Timestamp, isna
 
 from app import (
     toggle_user_guide_modal,
@@ -10,6 +11,8 @@ from app import (
     update_password_chart,
     update_public_chart,
     update_total_games_value,
+    update_game_duration_histogram,
+    update_table
 )
 
 
@@ -149,3 +152,159 @@ def test_toggle_modal(
         toggle_user_guide_modal(user_guide_button_clicks, close_button_clicks, is_modal_open)
         == expected_is_modal_open
     )
+
+
+def test_update_game_duration_histogram_returns_a_histogram_figure_given_table_columns_and_data():
+    # Arrange
+    fake_data = [
+        {'GAME_DURATION': 3, 'RELOAD': 0},
+        {'GAME_DURATION': 4.57, 'RELOAD': 1},
+        {'GAME_DURATION': 0, 'RELOAD': 0},
+        {'GAME_DURATION': 1.5, 'RELOAD': 0},
+        {'GAME_DURATION': 1000, 'RELOAD': 0},
+    ]
+    fake_columns = [
+        {'name': 'GAME_DURATION', 'id': 'GAME_DURATION'},
+        {'name': 'RELOAD', 'id': 'RELOAD'}
+    ]
+    expected_figure_trace_type = 'histogram'
+
+    # Act
+    figure = update_game_duration_histogram(fake_data, fake_columns)
+
+    # Assert
+    assert figure['data'][0].type == expected_figure_trace_type
+
+
+class TestUpdateTable:
+    def test_valid_dates_and_data_returns_list_of_records_with_a_derived_game_duration_field(self, mocker):
+        # Patch the database connection and cursor
+        mock_connect_to_mariadb = mocker.patch('app.connect_to_mariadb')
+        mock_connection = mocker.Mock()
+        mock_cursor = mocker.Mock()
+        mock_connect_to_mariadb.return_value = mock_connection
+        mock_connection.cursor.return_value = mock_cursor
+        mock_cursor.execute.return_value = None
+
+        # Fake schema
+        mock_cursor.description = [
+            ('INSTANCE_UUID', None),
+            ('GAME_ID', None),
+            ('INSTANCE_VERSION', None),
+            ('GAME_NAME', None),
+            ('START_TIME', None),
+            ('END_TIME', None),
+            ('REPLAY_NAME', None),
+            ('OOS', None),
+            ('RELOAD', None),
+            ('OBSERVERS', None),
+            ('PASSWORD', None),
+            ('PUBLIC', None)
+        ]
+
+        # Fake data
+        mock_cursor.fetchall.return_value = [(
+            '3f29e05d-8b16-11ec-81a2-0ee77816ca01',
+            4892,
+            '1.16',
+            'Partie de Kema_Eka',
+            # datetime(2022, 2, 17, 21, 22, 46),
+            # datetime(2022, 2, 17, 21, 25, 46),
+            '2022-02-17 21:22:46',
+            '2022-02-17 21:25:46',
+            None,
+            0,
+            0,
+            1,
+            1,
+            1
+        )]
+        total_games = 1000
+
+        # Query date range
+        start_date = '2022-02-01'
+        end_date = '2022-02-28'
+
+        expected_table_data = ([
+            {
+                'INSTANCE_UUID': '3f29e05d-8b16-11ec-81a2-0ee77816ca01',
+                'GAME_ID': 4892,
+                'INSTANCE_VERSION': '1.16',
+                'GAME_NAME': 'Partie de Kema_Eka',
+                'START_TIME': Timestamp('2022-02-17 21:22:46'),
+                'END_TIME': Timestamp('2022-02-17 21:25:46'),
+                'REPLAY_NAME': None,
+                'OOS': 0,
+                'RELOAD': 0,
+                'OBSERVERS': 1,
+                'PASSWORD': 1,
+                'PUBLIC': 1,
+                'GAME_DURATION': 3  # the unit is in minutes
+            }
+        ], False)
+
+        # Act
+        table_data = update_table(total_games, start_date, end_date)
+
+        # Assert
+        assert table_data == expected_table_data
+
+    def test_null_start_and_end_times_results_in_a_null_game_duration(self, mocker):
+        # Patch the database connection and cursor
+        mock_connect_to_mariadb = mocker.patch('app.connect_to_mariadb')
+        mock_connection = mocker.Mock()
+        mock_cursor = mocker.Mock()
+        mock_connect_to_mariadb.return_value = mock_connection
+        mock_connection.cursor.return_value = mock_cursor
+        mock_cursor.execute.return_value = None
+
+        # Fake schema
+        mock_cursor.description = [
+            ('START_TIME', None),
+            ('END_TIME', None),
+        ]
+
+        # Fake data
+        mock_cursor.fetchall.return_value = [(
+            NaT,
+            NaT,
+        )]
+        total_games = 1000
+
+        # Query date range
+        start_date = '2022-02-01'
+        end_date = '2022-02-28'
+
+        # Act
+        table_data = update_table(total_games, start_date, end_date)
+
+        # Assert
+        assert isna(table_data[0][0]['GAME_DURATION'])
+
+    @pytest.mark.parametrize(
+        "start_date, end_date",
+        [
+            (None, None),
+            (None, '2022-01-02'),
+            ('2022-01-01', None)
+        ]
+    )
+    def test_any_missing_dates_raises_prevent_update(self, start_date, end_date):
+        # Arrange
+        total_games = 5000
+
+        # Act and Assert
+        with pytest.raises(PreventUpdate):
+            update_table(total_games, start_date, end_date)
+
+    def test_exceeding_query_row_limit_toggles_popup_modal(self):
+        # Arrange
+        total_games = 10000
+        start_date = '2022-02-01'
+        end_date = '2022-02-28' 
+
+        # Act
+        returned_tuple = update_table(total_games, start_date, end_date) 
+
+        # Assert
+        returned_tuple[1] == True
